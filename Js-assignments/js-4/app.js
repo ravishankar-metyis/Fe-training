a2029579e8c44a720cd6d44369f573e63f3ab0bf
Ravi Shankar <ravishankar.satheesh@metyis.com>
2022-11-18 16:43:55 +0530

    Js assignment 4 - Able to delete and check off todo items, editing is WIP

--- Js-assignments/js-4/app.js
+++ Js-assignments/js-4/app.js
@@ -2,8 +2,6 @@ const todoInput = document.querySelector('#todoIn');
 const todoList = document.querySelector('#list-items');
 let todos = [];
 
-
-
 document.querySelector('#submit').addEventListener('click', (event) =>{
     event.preventDefault();
     addTodo(todoInput.value);
@@ -57,3 +55,51 @@ const getFromLocalStorage = () =>{
     }
 }
 getFromLocalStorage();
+
+
+// Check off, Edit & Delete Functionality
+
+
+todoList.addEventListener('click', function(event) {
+    if (event.target.type === 'checkbox') {
+        checkOff(event.target.parentElement.getAttribute('data-key'));
+  }
+  if (event.target.classList.contains('delete-button')) {
+    deleteTodo(event.target.parentElement.getAttribute('data-key'));
+  }
+  if (event.target.classList.contains('edit-button')) {
+    editTodo(event.target.parentElement.getAttribute('data-key'));
+  }
+});
+
+const checkOff = (id) =>{
+    todos.forEach((item) =>{
+        if (item.id == id) {
+      item.completed = !item.completed; //sets the completed value to the opposite (true/false)
+    }});
+    addToLocalStorage(todos);
+}
+
+const deleteTodo = (id) => {
+     todos = todos.filter((item) =>{
+    return item.id != id;
+  });
+  addToLocalStorage(todos);
+}
+
+
+const editTodo = (id) =>{
+
+    todos.forEach((item) =>{
+        if (item.id == id) {
+          const li = document.getElementById(item.id);
+          li.innerHTML = `<input type="text" calss="checkbox" value="${item.name} id="${id}">
+          <button class="okay-button">✔️</button>`;
+          todoList.addEventListener('click', function(event) {
+            if (event.target.classList.contains('okay-button')) {
+            displayTodo();
+  }               
+          });
+        }
+    })
+}
